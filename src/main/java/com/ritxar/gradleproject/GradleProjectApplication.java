74617503c6022465a07d317ca832a72165406fe2
rayaladeliras <rayaladeliras@stubhub.com>
2019-10-17 14:55:34 +0200

    test overwriting config in the test

--- src/main/java/com/ritxar/gradleproject/GradleProjectApplication.java
+++ src/main/java/com/ritxar/gradleproject/GradleProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,3 +23,61 @@ public class GradleProjectApplication {
 
 }
 
+@Configuration
+class MyConfig {
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean PersonClient personClient() {
+		return new HttpPersonClient(restTemplate());
+	}
+
+	@Bean PersonService personService() {
+		return new PersonService(personClient());
+	}
+}
+
+class PersonService {
+	private final PersonClient personClient;
+
+	PersonService (PersonClient personClient) {
+		this.personClient = personClient;
+	}
+
+	List<Person> people (){
+		List<Person> people = new ArrayList<>();
+		people.add(this.personClient.person(1));
+		people.add(this.personClient.person(2));
+		return people;
+	}
+}
+
+class Person {
+	String name, surname;
+
+	public Person (String name, String surname) {
+		this.name = name;
+		this.surname = surname;
+	}
+}
+
+interface PersonClient {
+	Person person(int id);
+}
+
+class HttpPersonClient implements  PersonClient {
+
+	private final RestTemplate restTemplate;
+
+	HttpPersonClient (RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	@Override
+	public Person person(int id) {
+		return this.restTemplate.getForObject(
+				"http://localhost:8081/person/{id}", Person.class, id);
+	}
+}
--- src/test/java/com/ritxar/gradleproject/GradleProjectApplicationTests.java
+++ src/test/java/com/ritxar/gradleproject/GradleProjectApplicationTests.java
@@ -3,11 +3,31 @@ package com.ritxar.gradleproject;
 import org.assertj.core.api.BDDAssertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class GradleProjectApplicationTests {
 
+	@Autowired PersonService personService;
 
+	@Test
+	public void should_get_two_people() {
+		List<Person> people = this.personService.people();
+		BDDAssertions.then(people).hasSize(2);
+	}
 
+	@Configuration
+	static class PersonTestConfiguration extends MyConfig {
+		@Override
+		PersonClient personClient () {
+			return id -> new Person ("m", "["+id+"]");
+		}
+	}
 }
